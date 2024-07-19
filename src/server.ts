f95c530cfef6ac055732ed54655659723064f899
Paulo Victor Rios <victorios.ba@gmail.com>
2024-07-19 09:27:04 -0300

    chore: client by server and by proxy

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,44 @@
-const configcat = require("configcat-node");
+import { getClient, IConfigCatClient, IEvaluationDetails, PollingMode, SettingTypeOf } from 'configcat-node'
+
+let configCatClient: IConfigCatClient;
+
+async function getValue(key: string, attempt: number) {
+  console.log(`Attempt ${attempt}`)
+
+  const response = await configCatClient.getValueDetailsAsync(key, undefined); 
+
+  console.log(`value ${response.value}`)
+
+  await wait(10000)
+
+  if (attempt % 2 === 0) {
+    await configCatClient.forceRefreshAsync()
+    console.log("Forcing refresh")
+  }
+}
+
+async function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 
 async function main() {
-  console.log(process.env.CONFIGCAT_KEY);
-  
-  const configCatClient = configcat.getClient(
-    process.env.CONFIGCAT_KEY, 
-    configcat.PollingMode.AutoPoll,
+  configCatClient = getClient(
+    process.env.CONFIGCAT_KEY_PROXY as string, 
+    PollingMode.AutoPoll,
     { baseUrl: "http://localhost:8050" }
   );
 
-  const value = await configCatClient.getValueAsync('ativar_opcao_1', undefined); 
-
-  if (value) {
-    console.log("New feature is enabled", value);
-  } else {
-    console.log("New feature is disabled", value); 
+  // configCatClient = getClient(
+  //   process.env.CONFIGCAT_KEY as string, 
+  //   PollingMode.AutoPoll,
+  // );
+  
+  let attempt = 1
+  while (attempt <= 10) {
+    await getValue("ativar_opcao_1", attempt)
+    attempt++
   }
-
-  configCatClient.dispose()
 }
 
 main()
